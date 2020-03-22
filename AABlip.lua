52adcb4c912e9b22ed3c89d15f49edd8ed824d43
Matti Vakkilainen <matti@vakkilainen.org>
2020-03-23 01:58:24 +0200

    initial commit

---
+++ AABlip.lua
@@ -0,0 +1,25 @@
+﻿local f = CreateFrame("Frame")
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+f:SetScript("OnEvent", function(self, event)
+    self:OnEvent(event, CombatLogGetCurrentEventInfo())
+end)
+
+function f:OnEvent(event, ...)
+	local timestamp, eventType, something, sourceFlags = ...
+	local sourceIdentifier = strsplit("-", sourceFlags);
+	local spellName = select(13, ...);
+
+	if (sourceIdentifier == "Player") then
+		if (eventType == "SWING_DAMAGE" or eventType == 'SWING_MISSED') then
+			PlaySoundFile("Interface\\AddOns\\AABlip\\blip.ogg", "Master");
+		end
+		if (eventType == 'SPELL_CAST_SUCCESS' and spellName == 'Heroic Strike') then
+			PlaySoundFile("Interface\\AddOns\\AABlip\\blip.ogg", "Master");
+		end
+		if (eventType == 'SPELL_CAST_SUCCESS' and spellName == 'Cleave') then
+			PlaySoundFile("Interface\\AddOns\\AABlip\\blip.ogg", "Master");
+		end
+	else
+		return;
+	end
+end
